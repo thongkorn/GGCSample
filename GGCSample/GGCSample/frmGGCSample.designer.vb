0aa9c3452424729b74df58f8ac7349f49193c2dc
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-08-21 14:25:55 +0700

    Add files via upload

---
+++ GGCSample/GGCSample/frmGGCSample.designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmGGCSample
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmGGCSample))
+        Me.GGC = New Syncfusion.Windows.Forms.Grid.Grouping.GridGroupingControl()
+        Me.btnExit = New Syncfusion.Windows.Forms.ButtonAdv()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.lblRecordCount = New System.Windows.Forms.Label()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        CType(Me.GGC, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GGC
+        '
+        Me.GGC.AlphaBlendSelectionColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(120, Byte), Integer), CType(CType(215, Byte), Integer))
+        Me.GGC.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GGC.BackColor = System.Drawing.SystemColors.Window
+        Me.GGC.Location = New System.Drawing.Point(0, 44)
+        Me.GGC.Name = "GGC"
+        Me.GGC.ShowCurrentCellBorderBehavior = Syncfusion.Windows.Forms.Grid.GridShowCurrentCellBorder.GrayWhenLostFocus
+        Me.GGC.Size = New System.Drawing.Size(935, 557)
+        Me.GGC.TabIndex = 3
+        Me.GGC.Text = "GridGroupingControl1"
+        Me.GGC.UseRightToLeftCompatibleTextBox = True
+        Me.GGC.VersionInfo = "15.2400.0.43"
+        '
+        'btnExit
+        '
+        Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnExit.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Metro
+        Me.btnExit.BackColor = System.Drawing.Color.FromArgb(CType(CType(17, Byte), Integer), CType(CType(158, Byte), Integer), CType(CType(218, Byte), Integer))
+        Me.btnExit.BeforeTouchSize = New System.Drawing.Size(86, 33)
+        Me.btnExit.BorderStyleAdv = Syncfusion.Windows.Forms.ButtonAdvBorderStyle.Dashed
+        Me.btnExit.ComboEditBackColor = System.Drawing.Color.Silver
+        Me.btnExit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.KeepFocusRectangle = False
+        Me.btnExit.Location = New System.Drawing.Point(845, 5)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Office2007ColorScheme = Syncfusion.Windows.Forms.Office2007Theme.Managed
+        Me.btnExit.Size = New System.Drawing.Size(86, 33)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit Program"
+        Me.btnExit.ThemeName = "Metro"
+        Me.btnExit.UseVisualStyle = True
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnRefresh.Image = CType(resources.GetObject("btnRefresh.Image"), System.Drawing.Image)
+        Me.btnRefresh.Location = New System.Drawing.Point(196, 11)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(24, 24)
+        Me.btnRefresh.TabIndex = 1
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label12.ForeColor = System.Drawing.Color.Black
+        Me.Label12.Location = New System.Drawing.Point(4, 15)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(56, 14)
+        Me.Label12.TabIndex = 60
+        Me.Label12.Text = "Search :"
+        '
+        'lblRecordCount
+        '
+        Me.lblRecordCount.AutoSize = True
+        Me.lblRecordCount.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblRecordCount.Location = New System.Drawing.Point(220, 15)
+        Me.lblRecordCount.Name = "lblRecordCount"
+        Me.lblRecordCount.Size = New System.Drawing.Size(17, 14)
+        Me.lblRecordCount.TabIndex = 59
+        Me.lblRecordCount.Text = "[]"
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtSearch.Location = New System.Drawing.Point(66, 12)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(130, 22)
+        Me.txtSearch.TabIndex = 0
+        '
+        'frmGGCSample
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(935, 602)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.lblRecordCount)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.GGC)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Name = "frmGGCSample"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Syncfusion GridGroupingControl - Code bY Thongkorn Tubtimkrob"
+        CType(Me.GGC, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GGC As Syncfusion.Windows.Forms.Grid.Grouping.GridGroupingControl
+    Private WithEvents btnExit As Syncfusion.Windows.Forms.ButtonAdv
+    Friend WithEvents btnRefresh As System.Windows.Forms.Button
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents lblRecordCount As System.Windows.Forms.Label
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+
+End Class
---
+++ GGCSample/GGCSample/frmGGCSample.vb
@@ -0,0 +1,203 @@
+﻿#Region "About"
+' / -----------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gnet.com/webboard
+' /
+' / Purpose: Sample code for GridGroupingControl of Syncfusion Community.
+' / Microsoft Visual Basic .NET (2010) & MS Access 2010.
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / -----------------------------------------------------------------
+#End Region
+
+'// Syncfusion
+Imports Syncfusion.Windows.Forms
+Imports Syncfusion.Windows.Forms.Grid
+Imports Syncfusion.Grouping
+Imports Syncfusion.Drawing
+'// DataBase
+Imports System.Data.OleDb
+
+Public Class frmGGCSample
+
+    Private Sub frmGGCSample_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        '// Connect DataBase
+        Call ConnectDataBase()
+        '// Show all records.
+        Call RetrieveData()
+    End Sub
+
+    ' / -----------------------------------------------------------------
+    ' / blnSearch = True, It's search for specified data, False = all data is displayed.
+    Private Sub RetrieveData(Optional ByVal blnSearch As Boolean = False)
+        strSQL = _
+            " SELECT Countries.CountryPK, Countries.A2, Countries.Country, Countries.Capital, " & _
+            " Countries.Population, Zones.ZoneName " & _
+            " FROM Countries INNER JOIN Zones ON Countries.ZoneFK = Zones.ZonePK "
+
+        '// blnSearch = True for Search
+        If blnSearch Then
+            strSQL = strSQL & _
+                " WHERE " & _
+                " [A2] " & " Like '%" & txtSearch.Text & "%'" & " OR " & _
+                " [Country] " & " Like '%" & txtSearch.Text & "%'" & " OR " & _
+                " [Capital] " & " Like '%" & txtSearch.Text & "%'" & " OR " & _
+                " [ZoneName] " & " Like '%" & txtSearch.Text & "%'" & _
+                " ORDER BY Country "
+        Else
+            strSQL = strSQL & " ORDER BY Country "
+        End If
+        '//
+        If Conn.State = ConnectionState.Closed Then Conn.Open()
+        '// Data Adapter. 
+        DA = New OleDbDataAdapter(strSQL, Conn)
+        ' Fill Data Set. 
+        DS = New DataSet
+        DA.Fill(DS)
+        Me.GGC.DataSource = DS.Tables(0)
+        lblRecordCount.Text = "[Total: " & Format(DS.Tables(0).Rows.Count, "#,##") & " Records.]"
+        DA.Dispose()
+        DS.Dispose()
+        Conn.Close()
+        '//
+        Call InitGridGroup()
+        '//
+        txtSearch.Clear()
+    End Sub
+
+    Private Sub btnRefresh_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRefresh.Click
+        '// Show all records.
+        Call RetrieveData(False)
+    End Sub
+
+    Private Sub txtSearch_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtSearch.KeyPress
+        If Trim(txtSearch.Text) = "" Or Len(Trim(txtSearch.Text)) = 0 Then Exit Sub
+        '// Undesirable characters for the database ex.  ', * or %
+        txtSearch.Text = Replace(Trim(txtSearch.Text), "'", "").Replace("%", "").Replace("*", "")
+        '/ RetrieveData(True) It means searching for information.
+        If e.KeyChar = Chr(13) Then '// Press Enter
+            '// No beep.
+            e.Handled = True
+            '// Search Data with True Parameter.
+            Call RetrieveData(True)
+        End If
+    End Sub
+
+    ' / -----------------------------------------------------------------
+    ' / Initilized GridGroupingControl
+    Private Sub InitGridGroup()
+        '// Initialize Columns GridGroup
+        With Me.GGC.TableDescriptor
+            '// Hidden Primary Key Column
+            .VisibleColumns.Remove("CountryPK")
+            '/ Using Column Name
+            .Columns("A2").HeaderText = "A2"
+            .Columns("Country").HeaderText = "Country"
+            .Columns("Capital").HeaderText = "Capital"
+            '// Format Population
+            With .Columns("Population")
+                .HeaderText = "Population"
+                .Appearance.AnyRecordFieldCell.CellValueType = GetType(Int32)
+                .Appearance.AnyRecordFieldCell.Format = "N0"
+            End With
+            .Columns("ZoneName").HeaderText = "Zone Name"
+        End With
+        '// GridVerticalAlignment.Middle
+        For i As Byte = 0 To 5
+            With Me.GGC.TableDescriptor
+                .Columns(i).Appearance.AnyRecordFieldCell.VerticalAlignment = GridVerticalAlignment.Middle
+                .Columns(i).AllowGroupByColumn = False
+                ' // Set Font any Columns.
+                .Columns(i).Appearance.AnyRecordFieldCell.Font = New Syncfusion.Windows.Forms.Grid.GridFontInfo(New Font("Tahoma", 11.0F, FontStyle.Regular))
+            End With
+        Next
+
+        '// Initialize normal GridGroupingControl
+        With Me.GGC
+            ' Allows GroupDropArea to be visible
+            .ShowGroupDropArea = False  ' Disable
+            '// Hidden Top Level of Grouping
+            .TopLevelGroupOptions.ShowCaption = False
+            '// Styles Theme
+            '// .GridVisualStyles = Syncfusion.Windows.Forms.GridVisualStyles.SystemTheme
+            '// Metro Styles
+            .GridVisualStyles = Syncfusion.Windows.Forms.GridVisualStyles.Office2010Blue
+            ' Disables editing in GridGroupingControl
+            .ActivateCurrentCellBehavior = GridCellActivateAction.None
+            ' Disables editing in GridGroupingControl
+            .TableDescriptor.AllowNew = False
+            '// Autofit Columns
+            .AllowProportionalColumnSizing = True
+
+            '// Row Height
+            .Table.DefaultRecordRowHeight = 25
+            '// 
+            .Table.DefaultCaptionRowHeight = 25
+            .Table.DefaultColumnHeaderRowHeight = 30    '// Columns Header Row Height
+
+            '// Selection
+            .TableOptions.ListBoxSelectionMode = SelectionMode.One
+            'Selection Back color
+            .TableOptions.SelectionBackColor = Color.Firebrick
+            '//
+            .Appearance.ColumnHeaderCell.TextColor = Color.DarkBlue
+
+            'Applies back color as LightCyan for alternative records in the Grid.
+            .Appearance.AlternateRecordFieldCell.BackColor = Color.LightCyan
+
+            'Disable record preview row 
+            .TableOptions.ShowRecordPreviewRow = False
+            'Will enable the Group Header for the top most group.
+            .TopLevelGroupOptions.ShowGroupHeader = False ' True
+            'Will enable the Group Footer for the group.
+            .TopLevelGroupOptions.ShowGroupFooter = False 'True
+            '//
+            .TableOptions.GroupHeaderSectionHeight = 30
+            .TableOptions.GroupFooterSectionHeight = 30
+        End With
+    End Sub
+
+    ' / -----------------------------------------------------------------
+    '// Double click event for show Primary Key which hidden in Column(0)
+    Private Sub GGC_TableControlCellDoubleClick(ByVal sender As Object, ByVal e As Syncfusion.Windows.Forms.Grid.Grouping.GridTableControlCellClickEventArgs) Handles GGC.TableControlCellDoubleClick
+        '// Row of Column Header 
+        If e.Inner.RowIndex <= 1 Then Return
+        '/ Notify the double click performed in a cell.
+        Dim rec As Record = Me.GGC.Table.DisplayElements(e.TableControl.CurrentCell.RowIndex).ParentRecord
+        If (rec) IsNot Nothing Then MessageBox.Show("Primary key = " & rec.GetValue("CountryPK").ToString)
+    End Sub
+
+    ' / -----------------------------------------------------------------
+    '// Full Select Row
+    Private Sub GGC_TableControlCurrentCellActivating(ByVal sender As Object, ByVal e As Syncfusion.Windows.Forms.Grid.Grouping.GridTableControlCurrentCellActivatingEventArgs) Handles GGC.TableControlCurrentCellActivating
+        '// Get Column Index 0 is the Primary Key. (Hidden column)
+        e.Inner.ColIndex = 0
+    End Sub
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    ' / -----------------------------------------------------------------
+    '// Press enter each row.
+    Private Sub GGC_TableControlCurrentCellKeyPress(sender As Object, e As Syncfusion.Windows.Forms.Grid.Grouping.GridTableControlKeyPressEventArgs) Handles GGC.TableControlCurrentCellKeyPress
+        '// Check rows count before.
+        If GGC.TableModel.RowCount <= 0 Then Return
+        '/ Notify the current cell keypress 
+        Dim rec As Record = Me.GGC.Table.DisplayElements(GGC.TableControl.CurrentCell.RowIndex).ParentRecord
+        If (rec) IsNot Nothing Then MessageBox.Show("Primary key = " & rec.GetValue("CountryPK").ToString)
+    End Sub
+
+    Private Sub frmGGCSample_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        If Conn.State = ConnectionState.Open Then Conn.Close()
+        Me.Dispose()
+        GC.SuppressFinalize(Me)
+        Application.Exit()
+    End Sub
+
+End Class
